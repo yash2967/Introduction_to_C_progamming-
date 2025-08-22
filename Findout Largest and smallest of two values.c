b80114d82451cd2f994875a1fdec2f9ed29a7884
yash2967 <pansuriyayash19@gmail.com>
2025-08-22 10:26:31 +0530

    Add files via upload

---
+++ Findout Largest and smallest of two values.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter Two Values");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+{
+    printf("a is greater");
+}
+else
+{
+    printf("b is greater");
+}
+}
